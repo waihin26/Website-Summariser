6d5758a7f0013406ae9eb4ece7a99abf05a5a7e6
Wai Hin <138366109+waihin26@users.noreply.github.com>
2025-01-01 13:47:17 +0800

    Edit template message

--- summariser.py
+++ summariser.py
@@ -32,7 +32,7 @@ if(user_input):
 
     template = """Use the following pieces of context to answer the question at the end. 
     If you don't know the answer, just say that you don't know, don't try to make up an answer. 
-    Use three sentences maximum. Return the result as a list. 
+    Use three sentences maximum. Try to keep it concise and accurate.
     {context}
     Question: {question}
     Helpful Answer:"""
